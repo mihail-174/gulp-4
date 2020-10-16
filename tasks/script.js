026708d16b6b44e010e33e77511a0d75a1849480
Михаил <dr.mihail@yandex.ru>
2020-10-16 14:42:10 +0500

    добавлена поддержка es6 и преобразование в старый формат

--- tasks/script.js
+++ tasks/script.js
@@ -1,29 +1,36 @@
 "use strict";
 
 import gulp from "gulp";
-import plumber from 'gulp-plumber';
-import errorHandler from 'gulp-plumber-error-handler';
-import cached from 'gulp-cached';
-import changed from 'gulp-changed';
-import uglify from 'gulp-uglify';
+import babel from "gulp-babel";
+import plumber from "gulp-plumber";
+import errorHandler from "gulp-plumber-error-handler";
+import cached from "gulp-cached";
+import changed from "gulp-changed";
+import uglify from "gulp-uglify";
 import rename from "gulp-rename";
 import debug from "gulp-debug";
+import sourcemaps from "gulp-sourcemaps";
 
 gulp.task("script", () => {
-    return gulp.src(['src/components/**/*.js', 'src/scripts/**/*.js', 'src/vendors/**/*.js'])
+    return gulp.src(["src/components/**/*.js", "src/scripts/**/*.js", "src/vendors/**/*.js"])
         .pipe(plumber({
-            errorHandler: errorHandler(`Ошибка в \'JS\' task`)
+            errorHandler: errorHandler("Ошибка в \'JS\' task")
         }))
-        .pipe(cached('js'))
-        .pipe(changed('src/js/', {
-            extension: '.js'
+        .pipe(cached("js"))
+        .pipe(changed("src/js/", {
+            extension: ".js"
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ["@babel/env"]
         }))
         .pipe(uglify())
         .pipe(rename({
             suffix: ".min",
-            dirname: '.'
+            dirname: "."
         }))
-        .pipe(gulp.dest('dist/js'))
+        .pipe(sourcemaps.write("./maps/"))
+        .pipe(gulp.dest("dist/js"))
         .pipe(debug({
             "title": "JS files"
         }));
